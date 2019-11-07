c83e1ace2a9cdbc2b3459662cbe366139aecfe9d
aljedaxi <aljedaxi@gmail.com>
2019-11-06 19:41:12 -0700

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,21 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxtjs
-      </h1>
-      <h2 class="subtitle">
-        My astonishing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+		<ul>
+			<nuxt-link to="/backmatter">backmatter</nuxt-link>
+			<nuxt-link to="/energy-sector">energy-sector</nuxt-link>
+			<nuxt-link to="/food-security">food-security</nuxt-link>
+			<nuxt-link to="/frontmatter">frontmatter</nuxt-link>
+			<nuxt-link to="/housing">housing</nuxt-link>
+			<nuxt-link to="/index">index</nuxt-link>
+			<nuxt-link to="/social-services">social-services</nuxt-link>
+			<nuxt-link to="/transit">transit</nuxt-link>
+		</ul>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
-}
+};
 </script>
 
 <style>
