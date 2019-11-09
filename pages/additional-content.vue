43034e5649be9790e447b7f99b0c87f6c7e1814a
aljedaxi <aljedaxi@gmail.com>
2019-11-09 16:19:48 -0700

    added Inure's edits

---
+++ pages/additional-content.vue
@@ -0,0 +1,166 @@
+<template>
+<div class="container">
+	<h2 id="engaging-experts">Engaging Experts</h2>
+	<ul>
+	<li>
+	<p>We need to identify local experts in each of the topics listed above, involve them meetings and organization as much as possible, so that they can answer questions at town halls, provide reliable information, and guide the revision and expansion of the GND towards the achievement of realistic, immediate goals.</p>
+	</li>
+	</ul>
+	<h2 id="addressing-concerns-about-jobs">Addressing Concerns about Jobs</h2>
+	<p>Federally focused studies</p>
+	<ul>
+	<li>
+	<p>(Federally focused) This study discusses how we can create over one million decent jobs in the public sector.</p>
+	</li>
+	<li>
+	<p>(Federally focused) “As many as 18,000 jobs are created for every $1 billion of investment in energy conservation and renewable energy systems. The average renewable energy investment creates four times as many jobs as the same investment in the fossil fuel economy.” Study</p>
+	</li>
+	<li>
+	<p>(Federally focused) This Columbia Institute study found that Canada can create 3.9 million direct jobs with net zero building retrofits and new “green” commercial, industrial, and institutional building construction: building small district energy systems, building an almost completely renewable energy grid by 2050, and expanding and improving our public transportation systems.</p>
+	</li>
+	<li>
+	<p>(Federally focused) This comprehensive study demonstrates the feasibility of federal free tuition and provides progressive fundraising mechanisms for presentation from a fiscally conservative.</p>
+	</li>
+	<li>
+	<p>“Every $1 million invested in efficiency programs generates 30 to 52 job-years.” [Source p. 13]</p>
+	</li>
+	<li>
+	<p>(Federally focused) This study discusses how we can create over one million decent jobs in the public sector.</p>
+	</li>
+	<li>
+	<p>(Federally focused) “As many as 18,000 jobs are created for every $1 billion of investment in energy conservation and renewable energy systems. The average renewable energy investment creates four times as many jobs as the same investment in the fossil fuel economy.” Study</p>
+	</li>
+	</ul>
+	<p>Provincially focused</p>
+	<ul>
+	<li>
+	<p>(Provincially focused) The 18,000 jobs per $1 billion of investment does not just apply to one area. Such investments can be done on a provincial basis, with paid student jobs and internships made a priority. Our engineering students and research infrastructure can easily be applied to a Green New Deal program.</p>
+	</li>
+	</ul>
+	<h3 id="on-pipelines">On pipelines</h3>
+	<ul>
+	<li>
+	<p>Environmental Concerns</p>
+
+	<ul>
+	<li>
+	<p>Canada and the world cannot meet any climate change targets if there is not an end to fossil fuel production.</p>
+	</li>
+	<li>
+	<p>There is no price advantage for bitumen in <a href="https://albertainnovates.ca/wp-content/uploads/2018/07/Bitumen-Partial-Upgrading-March-2018-Whitepaper-2433-Jacobs-Consultancy-FINAL_04July.pdf">Asia</a>.</p>
+	</li>
+	<li>
+	<p>Alberta has all of the pipeline capacity it needs, sells mostly to US markets, and <a href="https://thenarwhal.ca/myth-asian-market-alberta-oil/">seriously underuses the TransMountain pipeline at current capacity for shipments to Asia</a>.</p>
+	</li>
+	<li>
+	<p>Lighter, cheaper, closer crude is available to Asia via Russia, the Gulf countries, Iran, etc.</p>
+	</li>
+	</ul></li>
+	<li>
+	<p>Jobs</p>
+
+	<ul>
+	<li>
+	<p>Automation has made it so that even in the unlikely event of another oil “boom”, there would be <a href="https://www.huffingtonpost.ca/2017/03/01/oilsands-jobs-automation_n_15080318.html">far less work</a>.</p>
+	</li>
+	<li>
+	<p><a href="https://our-time.ca/stephen/">Testimony</a> from someone who works in the oil &amp; gas industry and who supports a Green New Deal.</p>
+	</li>
+	</ul></li>
+	<li>
+	<p>Taxes</p>
+
+	<ul>
+	<li>
+	<p>“Canadian corporations increased the money they report in Canada’s top 12 overseas tax havens by almost 10% to a record high of $353 billion in 2018, according to <a href="https://www150.statcan.gc.ca/n1/daily-quotidien/190424/dq190424a-eng.htm?HPA=1">Statistics Canada Foreign Direct Investment</a> figures released....” - <a href="https://www.taxfairness.ca/en/press_release/2019-04/canadian-corporate-cash-top-12-tax-havens-increases-10-all-time-high-2018">Canadians for Tax Fairness</a></p>
+	</li>
+	</ul></li>
+	</ul>
+	<h2 id="on-worker-cooperatives">On Worker Cooperatives</h2>
+	<p>Worker cooperatives are employee-owned businesses in which decisions are made democratically: one worker, one vote. Worker coops are an innovative model of keeping wealth local and incomes high, while ensuring the energy grid of the future is accountable to the community.</p>
+	<ul>
+	<li>
+	<p>Efficiency, feasibility, profitability, potential</p>
+
+	<ul>
+	<li>
+	<p>Worker coops are <a href="https://www.thenation.com/article/worker-cooperatives-are-more-productive-than-normal-companies/">more productive</a> than conventional firms.</p>
+	</li>
+	<li>
+	<p>A more in-depth <a href="https://www.uk.coop/sites/default/files/uploads/attachments/worker_co-op_report.pdf">study</a> on worker coops that discusses their size, profitability, viability in different sectors, etc.</p>
+	</li>
+	<li>
+	<p><a href="https://www.theguardian.com/world/2013/mar/07/mondragon-spains-giant-cooperative">Mondragon</a> is one of the most prominent worker coop examples: they compete globally, and have a university which welcomes students from GM and Microsoft.</p>
+	</li>
+	</ul></li>
+	<li>
+	<p>The <a href="https://www.youtube.com/watch?v=s_kLye_6VBc">Evergreen Model</a> is a prime example of how worker cooperatives can</p>
+
+	<ul>
+	<li>
+	<p>revive impoverished areas in an environmentally and economically sustainable way</p>
+	</li>
+	<li>
+	<p>naturally build wealth more equitably,</p>
+	</li>
+	<li>
+	<p>and provide a reliable tax base for local government.</p>
+	</li>
+	</ul></li>
+	<li>
+	<p>Policies</p>
+
+	<ul>
+	<li>
+	<p>Increased <a href="http://canadianworker.coop/wp-content/uploads/2012/06/CWCF_Research_Paper_International_16-6-2010_fnl1.pdf">supportive policies</a> must be made to encourage the growth of and educate people about worker cooperatives in order to build the sector.</p>
+	</li>
+	<li>
+	<p>Allow workers “the right of first refusal”: the right to refuse corporate buyout. <a href="https://www.jacobinmag.com/2019/04/worker-ownership-private-equity-cooperatives">article</a>.</p>
+	</li>
+	<li>
+	<p>More information may be requested by Students for Direct Action or the Canadian Worker Cooperative Federation on this topic.</p>
+	</li>
+	</ul></li>
+	</ul>
+	<h3 id="miscellaneous">Miscellaneous</h3>
+	<ul>
+	<li>
+	<p>Commercial, industrial, and institutional building construction: building small district energy systems, building an almost completely renewable energy grid by 2050, and expanding and improving our public transportation systems.</p>
+	</li>
+	<li>
+	<p>(Federally focused) This comprehensive <a href="https://cfs-fcee.ca/wp-content/uploads/2018/10/CFS-TimeToThinkBIG-Web-EN.pdf">study</a> demonstrates the feasibility of federal free tuition and provides progressive fundraising mechanisms for presentation from a fiscally conservative.</p>
+	</li>
+	<li>
+	<p>(Provincially focused) Free tuition is ideal for the facilitation of a Green New Deal program. Consider this <a href="https://cfs-fcee.ca/wp-content/uploads/2018/10/CFS-TimeToThinkBIG-Web-EN.pdf">study</a> (p. 15) which finds that total public education (college, vocational, university all included) revenue amounts to about $918 million. The province can pass legislation mandating the elimination of tuition fees for students that go to public institutions. How it <a href="https://www.thestar.com/calgary/2019/04/04/just-the-facts-checking-ndp-leader-rachel-notleys-election-stump-statements.html">could</a> be <strong>paid for</strong> would be for an increase in corporate taxes of between 1.5-2%, using the following <a href="https://www.thestar.com/calgary/2019/04/04/just-the-facts-checking-ndp-leader-rachel-notleys-election-stump-statements.html">information</a>: “The NDP says that corporate taxes generate $316 million per tax point…” &amp; “the net reduction from 2001-2006, when the corporate tax rate dropped by 5.5 per cent, saw the province lose $3.7 billion.”</p>
+	</li>
+	<li>
+	<p>Other Justice Issues and Supplementary economic visions:</p>
+
+	<ul>
+	<li>
+	<p>Eliminate homelessness like <a href="https://www.cbc.ca/news/canada/calgary/medicine-hat-homeless-free-update-1.3949030">Medicine Hat</a><br />
+	(even with a fiscally conservative city council).</p>
+	</li>
+	<li>
+	<p>Medicare and pharmacare-for-all</p>
+	</li>
+	<li>
+	<p>Free public transit <a href="https://www.theguardian.com/world/2018/feb/14/german-cities-to-trial-free-public-transport-to-cut-pollution">like</a> Germany</p>
+	</li>
+	<li>
+	<p>Food and land sovereignty with projects like <a href="https://www.growcalgary.ca/about-us/">Grow Calgary</a></p>
+	</li>
+	</ul></li>
+	</ul>
+	<p></p>
+</div>
+</template>
+
+<script>
+export default {
+	layout: 'article',
+};
+</script>
+
+<style>
+</style>
