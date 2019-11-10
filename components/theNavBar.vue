37136f422cab100e7e2569995c38899b22c2b5fb
Patrick King <patrick.f.king@gmail.com>
2019-11-09 22:45:06 -0700

    Hide menu behind a button on mobile.

--- components/theNavBar.vue
+++ components/theNavBar.vue
@@ -1,28 +1,61 @@
 <template>
-	<nav id="top-bar">
-    <div class="title">
-  		<span class="title-title">Green New Deal Maximum Program</span>
-  		<span class="title-subtitle">
-    		<em>for Calgary, Alberta</em>
-  		</span>
+  <div>
+    <nav id="top-bar">
+      <div class="title">
+        <span class="title-title">Green New Deal Maximum Program</span>
+        <span class="title-subtitle">
+          <em>for Calgary, Alberta</em>
+        </span>
+      </div>
+      <div class="nav full-width">
+        <span class="nav-item"><nuxt-link to="/">
+          Home
+        </nuxt-link></span>
+        <span class="nav-item"><nuxt-link to="/toc">
+          The Green New Deal
+        </nuxt-link></span>
+        <span class="nav-item"><nuxt-link to="/more">
+          More
+        </nuxt-link></span>
+      </div>
+      <div class="nav mobile-width">
+        <span class="nav-item">
+          <a href="#" v-on:click="toggleMobileNav">Menu</a>
+        </span>
+      </div>
+    </nav>
+    <div class="nav mobile-width" v-if="showMobileNav">
+      <p class="nav-item"><nuxt-link to="/">
+        Home
+      </nuxt-link></p>
+      <br/>
+      <p class="nav-item"><nuxt-link to="/toc">
+        The Green New Deal
+      </nuxt-link></p>
+      <br/>
+      <p class="nav-item"><nuxt-link to="/more">
+        More
+      </nuxt-link></p>
     </div>
-		<div id="nav">
-			<span class="nav-item"><nuxt-link to="/">
-				Home
-			</nuxt-link></span>
-			<span class="nav-item"><nuxt-link to="/toc">
-				The Green New Deal
-			</nuxt-link></span>
-			<span class="nav-item"><nuxt-link to="/more">
-				More
-			</nuxt-link></span>
-		</div>
-	</nav>
+    <div class="mobile-width mobile-ruler"></div>
+  </div>
 </template>
 
 <script>
-	export default {
-	};
+  export default {
+    data: () => {
+      return {
+        showMobileNav: false,
+      }
+    },
+    methods: {
+      toggleMobileNav: function(e) {
+        e.preventDefault()
+        console.log(this)
+        this.showMobileNav = !this.showMobileNav
+      } 
+    }
+  };
 </script>
 
 <style>
