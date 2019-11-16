026dd65b17cebd0c4496b63c4d3e63e1077f0e88
aljedaxi <aljedaxi@gmail.com>
2019-11-16 16:45:30 -0700

    post townhall changes

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
 	<p>This <em><b>Green New Deal Maximum Program</b></em> is a collection of evidence based, actionable proposals. As a Maximum Program, this set of ideas is highly ambitious; we hope they can serve as a starting point for achievable intermediate Green New Deal programs.
 	</p>
 
+	<!--
     <p> You are invited to join us at a <a href="https://www.facebook.com/events/410992476249334/">public presentation and townhall</a> to learn about the Green New Deal and get involved expanding it.
     </p>
     <p>
@@ -13,8 +14,10 @@
       1133 7 Ave SW, Calgary, AB<br>
       2nd floor lecture hall<br>
     </p>
+	-->
 
 	<p>You can read the program <nuxt-link to="/toc">online</nuxt-link>, or download the <a href="/yyc_gnd_mp.pdf">pdf</a>.</p>
+	<p>You can contribute to the document at our <a href="https://municipalgreennewdeal.miraheze.org/wiki/Main_Page">GND Maximum Program Wiki</a></p>
 
 	<!-- 
 	<img style="width: 100%;" src="/front_cover.svg"/>
