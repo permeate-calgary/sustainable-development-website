05959a01c13aefea6d1410d425b4c5c3bd34715e
aljedaxi <aljedaxi@gmail.com>
2019-11-21 01:14:23 -0700

    added social media

--- pages/more.vue
+++ pages/more.vue
@@ -1,30 +1,45 @@
 <template>
   <div class="container">
-    <h2>More</h2>
+    <h2>Get in Contact</h2>
 
-		<a href="mailto:permeatecalgary@gmail.com">
-			<h3>Contact us</h3>
-		</a>
+			<a href="mailto:permeatecalgary@gmail.com">
+				<h3>Email</h3>
+			</a>
 
-		<a href="/yyc_gnd_mp.pdf">
-			<h3>Download the Green New Deal (PDF)</h3>
-		</a>
+			<a href="https://www.instagram.com/permeatecalgary/">
+				<h3>Instagram</h3>
+			</a>
 
-		<a href="https://drive.google.com/open?id=1IjoNnJFl2Ez3w1OGbEbBkJYVL25CozU4">
-			<h3>See the Townhall Presentation Slide Deck</h3>
-		</a>
+			<a href="https://www.facebook.com/permeatecalgary/">
+				<h3>Facebook</h3>
+			</a>
 
-		<a href="/flyer_front.jpg">
-			<h3>Townhall Event Flyer Front (JPG)</h3>
-		</a>
+			<a href="https://www.twitter.com/permeatecalgary/">
+				<h3>Twitter</h3>
+			</a>
 
-		<a href="/flyer_reverse.jpg">
-			<h3>Townhall Event Flyer Reverse (JPG)</h3>
-		</a>
-	
-		<a href="/Organizers launch Calgarys Green New Deal - Press Release.pdf">
-			<h3>Townhall Press Release, November 11 2019 (PDF)</h3>
-		</a>
+    <h2>Downloadable Resources</h2>
+
+			<a href="/yyc_gnd_mp.pdf">
+				<h3>Download the Green New Deal (PDF)</h3>
+			</a>
+
+			<a href="https://drive.google.com/open?id=1IjoNnJFl2Ez3w1OGbEbBkJYVL25CozU4">
+				<h3>See the Townhall Presentation Slide Deck</h3>
+			</a>
+
+			<a href="/flyer_front.jpg">
+				<h3>Townhall Event Flyer Front (JPG)</h3>
+			</a>
+
+			<a href="/flyer_reverse.jpg">
+				<h3>Townhall Event Flyer Reverse (JPG)</h3>
+			</a>
+		
+			<a href="/Organizers launch Calgarys Green New Deal - Press Release.pdf">
+				<h3>Townhall Press Release, November 11 2019 (PDF)</h3>
+			</a>
+
   </div>
 </template>
 
