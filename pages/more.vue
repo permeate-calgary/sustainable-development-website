ebf94a4fee3775ddd9e8d4567af6bab67dd23b98
aljedaxi <aljedaxi@gmail.com>
2019-11-21 00:58:52 -0700

    Added Presentation powerpoint

--- pages/more.vue
+++ pages/more.vue
@@ -2,26 +2,29 @@
   <div class="container">
     <h2>More</h2>
 
-      <a href="/yyc_gnd_mp.pdf">
-        <h3>Download the Green New Deal (PDF)</h3>
-      </a>
+		<a href="mailto:permeatecalgary@gmail.com">
+			<h3>Contact us</h3>
+		</a>
 
-      <a href="/flyer_front.jpg">
-        <h3>Townhall Event Flyer Front (JPG)</h3>
-      </a>
+		<a href="/yyc_gnd_mp.pdf">
+			<h3>Download the Green New Deal (PDF)</h3>
+		</a>
 
-      <a href="/flyer_reverse.jpg">
-        <h3>Townhall Event Flyer Reverse (JPG)</h3>
-      </a>
-    
-      <a href="/Organizers launch Calgarys Green New Deal - Press Release.pdf">
-        <h3>Townhall Press Release, November 11 2019 (PDF)</h3>
-      </a>
+		<a href="https://drive.google.com/open?id=1IjoNnJFl2Ez3w1OGbEbBkJYVL25CozU4">
+			<h3>See the Townhall Presentation Slide Deck</h3>
+		</a>
 
-      <a href="mailto:permeatecalgary@gmail.com">
-        <h3>Contact us</h3>
-      </a>
+		<a href="/flyer_front.jpg">
+			<h3>Townhall Event Flyer Front (JPG)</h3>
+		</a>
 
+		<a href="/flyer_reverse.jpg">
+			<h3>Townhall Event Flyer Reverse (JPG)</h3>
+		</a>
+	
+		<a href="/Organizers launch Calgarys Green New Deal - Press Release.pdf">
+			<h3>Townhall Press Release, November 11 2019 (PDF)</h3>
+		</a>
   </div>
 </template>
 
